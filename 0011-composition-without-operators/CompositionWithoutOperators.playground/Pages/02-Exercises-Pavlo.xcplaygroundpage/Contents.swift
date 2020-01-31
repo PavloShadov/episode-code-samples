9d1f0fb51dfe70af69c665c7190f2d42b33a839f
PavloShadov <pavlo.shadov@gmail.com>
2020-01-31 22:51:57 +0100

    Pavlo: finished 0011

--- 0011-composition-without-operators/CompositionWithoutOperators.playground/Pages/02-Exercises-Pavlo.xcplaygroundpage/Contents.swift
+++ 0011-composition-without-operators/CompositionWithoutOperators.playground/Pages/02-Exercises-Pavlo.xcplaygroundpage/Contents.swift
@@ -22,6 +22,7 @@ func concat<A>(_ f: @escaping (A) -> A,
 //    THIS SOLUTION DOESN'T WORK BECAUSE OF ONE ADDITIONAL FUNCTION CALL
 //    guard let firstVariadicFunc = fs.first else { return a |> f >>> g }
 //    return a |> f >>> g >>> fs.reduce(firstVariadicFunc, >>>)
+    
     let allFunctions = [f, g] + fs
     return allFunctions.reduce(a, { result, element in element(result) })
   }
@@ -31,4 +32,45 @@ concat(incr, incr, incr, incr)(3)
 /*:
  3. Write compose for backward composition. Recreate some of the examples from our functional setters episodes (part 1 and part 2) using compose and pipe.
  */
-// TODO
+func pipe<A, B, C>(_ f: @escaping (A) -> B, _ g: @escaping (B) -> C) -> (A) -> C {
+  return { g(f($0)) }
+}
+
+func backPipe<A, B, C>(_ f: @escaping (B) -> C, _ g: @escaping (A) -> B) -> (A) -> C {
+  return { f(g($0)) }
+}
+
+func with<A, B>(_ a: A, _ f: (A) -> B) -> B {
+  return f(a)
+}
+
+func second<A, B, C>(_ f: @escaping (B) -> C) -> ((A, B)) -> (A, C) {
+  return { ($0.0, f($0.1)) }
+}
+
+func first<A, B, C>(_ f: @escaping (A) -> B) -> ((A, C)) -> (B, C) {
+  return { (f($0.0), $0.1) }
+}
+
+func map<A, B>(_ f: @escaping(A) -> B) -> ([A]) -> [B] {
+  return { $0.map(f) }
+}
+
+
+let targetOne = ((1, "One"), false)
+
+//targetOne
+//  |> (first <<< first)(incr)
+//  |> (first <<< second) { $0.uppercased() }
+//  |> second { !$0 }
+
+with(targetOne,
+  pipe(
+    pipe(
+      backPipe(first, first)(incr),
+      backPipe(first, second)({ $0 + "!" })
+    ),
+    second { !$0 }
+  )
+)
+
